fba4be678b49220923d7243e55ead631b5b8119f
AnnaRychkova1 <aanytkaa@gmail.com>
2024-03-09 10:14:16 +0000

    todo-task-2

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,3 +1,13 @@
-export const TodoList = () => {
-  return <h3>TodoList</h3>;
+import { Grid, GridItem, TodoListItem } from '..';
+
+export const TodoList = ({ array }) => {
+  return (
+    <Grid>
+      {array.map((item, index) => (
+        <GridItem key={item.id}>
+          <TodoListItem {...item} count={index + 1} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,3 +1,17 @@
-export const TodoListItem = () => {
-  return <h3>TodoListItem</h3>;
+import { Text } from '..';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import style from './TodoListItem.module.css';
+
+export const TodoListItem = ({ id, text, count }) => {
+  return (
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        {count}
+      </Text>
+      <Text>{text}</Text>
+      <button className={style.deleteButton} type="button">
+        <RiDeleteBinLine size={24} />
+      </button>
+    </div>
+  );
 };
