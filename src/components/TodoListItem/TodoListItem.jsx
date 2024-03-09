d27d2b472e0f9df348f1a50e96b99cd1991baa35
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-03-09 12:47:22 +0200

    end task 3

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,14 +2,18 @@ import { Text } from '..';
 import { RiDeleteBinLine } from 'react-icons/ri';
 import style from './TodoListItem.module.css';
 
-export const TodoListItem = ({ id, text, count }) => {
+export const TodoListItem = ({ id, text, count, deletTodo }) => {
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
         {count}
       </Text>
       <Text>{text}</Text>
-      <button className={style.deleteButton} type="button">
+      <button
+        className={style.deleteButton}
+        type="button"
+        onClick={() => deletTodo(id)}
+      >
         <RiDeleteBinLine size={24} />
       </button>
     </div>
