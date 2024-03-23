e75b0c43ec0482c5e57e93696bf8fe6702a5863b
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-03-23 15:23:17 +0200

    task1

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,19 @@
-import { Text } from 'components';
+import { Form } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+  const addTodo = text => {
+    const toDo = {
+      text,
+      id: nanoid(),
+    };
+    setTodos([...todos, toDo]);
+  };
+  return (
+    <>
+      <Form addTodo={addTodo} />
+    </>
+  );
 };
