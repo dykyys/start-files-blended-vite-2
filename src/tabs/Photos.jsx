87b6f1f1e5eb226db7a50a86a03ebf72cc3cd3e1
Zakhar-T <zakhar.null@gmail.com>
2025-08-18 14:52:57 +0300

    final edit

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -13,36 +13,40 @@ const Photos = () => {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  let isEndOfGallery = useRef(false);
+
+  const isEndOfGallery = useRef(false);
 
   useEffect(() => {
     if (query === '') return;
 
     async function uploadGallery() {
-      setIsLoading(true);
       try {
+        setIsError(false);
+        setIsLoading(true);
         const data = await getPhotos(query, page);
-        const { per_page, total_results, photos } = data;
-        setImages(images => {
-          return [...images, ...photos];
+        const { photos, per_page, total_results } = data;
+        setImages(prevPhotos => {
+          return [...prevPhotos, ...photos];
         });
-        total_results / per_page > 1
-          ? (isEndOfGallery.current = false)
-          : (isEndOfGallery.current = true);
-        console.log(total_results / per_page > 1);
-      } catch (error) {
+        if (total_results - per_page * page < 0) {
+          isEndOfGallery.current = true;
+        } else {
+          isEndOfGallery.current = false;
+        }
+      } catch {
         setIsError(true);
-        console.log(error);
       } finally {
         setIsLoading(false);
       }
     }
+
     uploadGallery();
   }, [query, page]);
 
   const onSearch = query => {
     setQuery(query);
     setPage(1);
+    setImages([]);
   };
 
   const uploadMorePhotos = () => {
